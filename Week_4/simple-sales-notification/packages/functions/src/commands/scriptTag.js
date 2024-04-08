7074376e1669d71f9f640bec8d4d0a2ef42d80f5
ThuanHipp <88172491+VanThuan76@users.noreply.github.com>
2024-04-08 23:18:44 +0700

    #fix: thuanvv fix security shopify

--- Week_4/simple-sales-notification/packages/functions/src/commands/scriptTag.js
+++ Week_4/simple-sales-notification/packages/functions/src/commands/scriptTag.js
@@ -1,12 +1,6 @@
 const Shopify = require('shopify-api-node');
 
 (async () => {
-  const shopify = new Shopify({
-    accessToken: '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',
-    shopName: 'avada-simple-sales-notfication.myshopify.com'
-  });
-  const scriptTags = await shopify.scriptTag.list();
-  console.log(scriptTags);
   // await shopify.scriptTag.create({
   //   event: 'onload',
   //   src: 'https://localhost:3000/scripttag/avada-sale-pop.min.js'
